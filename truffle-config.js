045bf13014567eaf10b33c3b22577ec4addd8bb8
Jose Fco de la Fuente <josefcodelafuente@gmail.com>
2021-05-01 14:04:38 +0200

    add test network

--- truffle-config.js
+++ truffle-config.js
@@ -11,6 +11,11 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    test: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
     kovan: {
       provider: function() {
         return new HDWalletProvider(
